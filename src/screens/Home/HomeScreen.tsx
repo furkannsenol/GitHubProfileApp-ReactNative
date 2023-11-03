dfc7cad6b810eff81a864b1dbf3d75915be23c0b
Furkan Şenol <frkn.snl@hotmail.com>
2023-11-04 02:18:42 +0300

    added initial pages

---
+++ src/screens/Home/HomeScreen.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import styles from './HomeStyles'
+
+const HomeScreen = () => {
+  return (
+    <View>
+      
+      <Text>HomeScreen</Text>
+    </View>
+  )
+}
+
+export default HomeScreen
---
+++ src/screens/UserNetwork/UserNetworkScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const UserNetworkScreen = () => {
+  return (
+    <View>
+      <Text>UserNetworkScreen</Text>
+    </View>
+  )
+}
+
+export default UserNetworkScreen
