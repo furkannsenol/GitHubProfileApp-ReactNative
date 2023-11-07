7103bc6259ee569df98f8266197e031ebb625573
Furkan Şenol <frkn.snl@hotmail.com>
2023-11-07 13:58:14 +0300

    changed statusbar styles

--- App.tsx
+++ App.tsx
@@ -6,10 +6,12 @@ import { Provider } from 'react-redux';
 
 import TabMain from './src/navigation/tab/TabMain';
 import store from './src/redux/configureStore';
+import { StatusBar } from 'react-native';
 
 function App() {
   return (
     <Provider store={store}>
+      <StatusBar barStyle={'light-content'} backgroundColor={'#238636'} />
       <TabMain />
     </Provider>
   );
