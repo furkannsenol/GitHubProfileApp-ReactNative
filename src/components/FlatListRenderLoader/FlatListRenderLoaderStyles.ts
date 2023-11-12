b29a83def087e671e1d86f34f888c987299d53e1
Furkan Şenol <frkn.snl@hotmail.com>
2023-11-12 16:34:34 +0300

    Create FlatListRenderLoaderStyles.ts

---
+++ src/components/FlatListRenderLoader/FlatListRenderLoaderStyles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        bottom: 0,
+        marginBottom: 10
+    }
+
+})
+
+export default styles
