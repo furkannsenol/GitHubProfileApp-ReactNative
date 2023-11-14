52140759141764de17ff1385a6f4b054cbd13606
Furkan Şenol <frkn.snl@hotmail.com>
2023-11-15 02:08:10 +0300

    removed skeleton for performance

--- src/screens/UserDetail/UserDetailScreen.tsx
+++ src/screens/UserDetail/UserDetailScreen.tsx
@@ -5,7 +5,7 @@ import { HomeTabParamList, RootStackParamList } from '../../navigation/navigatio
 import SkeletonContent from 'react-native-skeleton-content-nonexpo';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Animated, { BounceInLeft, BounceInRight, FadeInUp, FlipInEasyX, LightSpeedInLeft, LightSpeedInRight, LightSpeedOutLeft, RotateInUpRight, SlideInRight, SlideInUp, ZoomInEasyDown, ZoomInRight, ZoomInRotate } from 'react-native-reanimated';
-
+import Spinner from 'react-native-spinkit';
 import { userDetailGetData } from '../../hooks/userDetail/userDetailGetDataHook' //user data retrieved from the hook
 
 import styles from './UserDetailStyles'
@@ -14,6 +14,7 @@ import UserImageModal from '../../components/UserImageModal/UserImageModalCompan
 import { userDetailGetStarredCount } from '../../hooks/userDetail/userDetailGetStarredCountHook';
 import { openInAppBrowser } from '../../utils/openInAppBrowser';
 import { userDetailLayout } from '../../layouts/UserDetailLayout';
+import { windowWidth } from '../../utils/constants';
 
 
 type Props = NativeStackScreenProps<HomeTabParamList, 'UserDetailScreen'>
@@ -38,88 +39,97 @@ const UserDetailScreen: React.FC<Props> = ({ route }) => {
   //Modal useState
   const [isImageModalVisible, setIsImageModalVisible] = useState(false);
 
+  if (isLoading || starredIsLoading) {
+    return (
+      <SafeAreaView style={{ flex: 1, alignItems: 'center', justifyContent: 'center',backgroundColor:'#0D1117'}}>
+        <Spinner isVisible={true} size={windowWidth/10} type='WordPress' color='white' />
+      </SafeAreaView>
+    );
+  }
+
   return (
     <SafeAreaView style={styles.mainContainer}>
-      <SkeletonContent
+      {/* <SkeletonContent
         containerStyle={styles.mainContainer}
         isLoading={isLoading || starredIsLoading}
         // isLoading={true}
         animationDirection='horizontalRight'
         layout={userDetailLayout}
         duration={2000}
-      >
-        <View style={styles.headerContainer}>
-          <View style={styles.headerButtonsContent}>
-            <Pressable onPress={() => turnBack()}>
-              <MaterialCommunityIcons name='chevron-left' size={24} color={'white'} style={styles.headerButtonBackground} />
-            </Pressable>
-            <MaterialCommunityIcons name='heart' size={24} color={'white'} style={styles.headerButtonBackground} />
-          </View>
+      > */}
 
-          <View style={styles.headerContent}>
-            <TouchableOpacity onPress={() => setIsImageModalVisible(true)}>
-              <Animated.Image
-                entering={BounceInLeft.duration(600)}
-                source={data?.avatar_url ? { uri: data.avatar_url } : require('../../assets/images/no_image_available.jpg')}
-                style={styles.headerImage}
-              />
-            </TouchableOpacity>
-            <Animated.View entering={BounceInRight.duration(600)} style={styles.headerTextContent}>
-              <Text style={styles.headerTextName}>{data?.name || 'Anonymous'}</Text>
-              <Text style={styles.headerTextLogin}>@{data?.login}</Text>
-              <MaterialCommunityIcons name='map-marker' color={'white'} size={13} >
-                <Text > {data?.location || 'Location not available'}</Text></MaterialCommunityIcons>
-            </Animated.View>
-          </View>
-
-          <View style={styles.headerBioContent}>
-            <Animated.Text style={styles.headerBioText} entering={FlipInEasyX.duration(600)}>
-              {data?.bio && data.bio.length > 90
-                ? `"${data.bio.slice(0, 90)}..."`
-                : data?.bio || 'Bio not available'}</Animated.Text>
-          </View>
-
-          <Animated.View style={styles.headerSummaryContainer} entering={FlipInEasyX.duration(600)}>
-            <View style={styles.headerSummaryContent}>
-              <Text style={styles.headerSummaryNumber}>{data?.public_repos}</Text>
-              <Text style={styles.headerSummaryText}>Public Repos</Text>
-            </View>
-
-            <View style={styles.headerSummaryBorder} />
-
-            <View style={styles.headerSummaryContent}>
-              <Text style={styles.headerSummaryNumber}>{starred || 0}</Text>
-              <Text style={styles.headerSummaryText}>Starred</Text>
-            </View>
-
-            <View style={styles.headerSummaryBorder} />
-
-            <View style={styles.headerSummaryContent}>
-              <Text style={styles.headerSummaryNumber}>{data?.public_gists}</Text>
-              <Text style={styles.headerSummaryText}>Public Gists</Text>
-            </View>
-          </Animated.View>
-
-          <Animated.View style={styles.headerButton} entering={SlideInUp.duration(750)}>
-            <Button title='VIEW PROFILE' color={'#238636'} onPress={() => openInAppBrowser(data?.html_url)} />
-          </Animated.View>
-
+      <View style={styles.headerContainer}>
+        <View style={styles.headerButtonsContent}>
+          <Pressable onPress={() => turnBack()}>
+            <MaterialCommunityIcons name='chevron-left' size={24} color={'white'} style={styles.headerButtonBackground} />
+          </Pressable>
+          <MaterialCommunityIcons name='heart' size={24} color={'white'} style={styles.headerButtonBackground} />
         </View>
 
-
-        <View style={styles.bodyContainer}>
-          <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 20 }}>
-            <Animated.Text entering={LightSpeedInLeft.duration(1000)}>Followers ({data?.followers})</Animated.Text>
-            <Animated.Text entering={LightSpeedInRight.duration(1000)}>View All</Animated.Text>
-          </View>
-          <View style={{ width: 75, height: 75, borderRadius: 75, backgroundColor: 'red', marginTop: 10 }} />
-          <Text style={{ marginTop: 5 }}>Deneme</Text>
-          {/* <Text style={{ flex: 1, backgroundColor: 'red', textAlign: 'center',marginBottom:20, position: 'absolute', bottom: 0, width:'100%',marginHorizontal:20 }}>{data?.created_at}</Text> */}
+        <View style={styles.headerContent}>
+          <TouchableOpacity onPress={() => setIsImageModalVisible(true)}>
+            <Animated.Image
+              entering={BounceInLeft.duration(600)}
+              source={data?.avatar_url ? { uri: data.avatar_url } : require('../../assets/images/no_image_available.jpg')}
+              style={styles.headerImage}
+            />
+          </TouchableOpacity>
+          <Animated.View entering={BounceInRight.duration(600)} style={styles.headerTextContent}>
+            <Text style={styles.headerTextName}>{data?.name || 'Anonymous'}</Text>
+            <Text style={styles.headerTextLogin}>@{data?.login}</Text>
+            <MaterialCommunityIcons name='map-marker' color={'white'} size={13} >
+              <Text > {data?.location || 'Location not available'}</Text></MaterialCommunityIcons>
+          </Animated.View>
         </View>
 
-        <UserImageModal visible={isImageModalVisible} imageUrl={data?.avatar_url} onClose={() => setIsImageModalVisible(false)} />
+        <View style={styles.headerBioContent}>
+          <Animated.Text style={styles.headerBioText} entering={FlipInEasyX.duration(600)}>
+            {data?.bio && data.bio.length > 90
+              ? `"${data.bio.slice(0, 90)}..."`
+              : data?.bio || 'Bio not available'}</Animated.Text>
+        </View>
 
-      </SkeletonContent>
+        <Animated.View style={styles.headerSummaryContainer} entering={FlipInEasyX.duration(600)}>
+          <View style={styles.headerSummaryContent}>
+            <Text style={styles.headerSummaryNumber}>{data?.public_repos}</Text>
+            <Text style={styles.headerSummaryText}>Public Repos</Text>
+          </View>
+
+          <View style={styles.headerSummaryBorder} />
+
+          <View style={styles.headerSummaryContent}>
+            <Text style={styles.headerSummaryNumber}>{starred || 0}</Text>
+            <Text style={styles.headerSummaryText}>Starred</Text>
+          </View>
+
+          <View style={styles.headerSummaryBorder} />
+
+          <View style={styles.headerSummaryContent}>
+            <Text style={styles.headerSummaryNumber}>{data?.public_gists}</Text>
+            <Text style={styles.headerSummaryText}>Public Gists</Text>
+          </View>
+        </Animated.View>
+
+        <Animated.View style={styles.headerButton} entering={SlideInUp.duration(750)}>
+          <Button title='VIEW PROFILE' color={'#238636'} onPress={() => openInAppBrowser(data?.html_url)} />
+        </Animated.View>
+
+      </View>
+
+
+      <View style={styles.bodyContainer}>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 20 }}>
+          <Animated.Text entering={LightSpeedInLeft.duration(1000)}>Followers ({data?.followers})</Animated.Text>
+          <Animated.Text entering={LightSpeedInRight.duration(1000)}>View All</Animated.Text>
+        </View>
+        <View style={{ width: 75, height: 75, borderRadius: 75, backgroundColor: 'red', marginTop: 10 }} />
+        <Text style={{ marginTop: 5 }}>Deneme</Text>
+        {/* <Text style={{ flex: 1, backgroundColor: 'red', textAlign: 'center',marginBottom:20, position: 'absolute', bottom: 0, width:'100%',marginHorizontal:20 }}>{data?.created_at}</Text> */}
+      </View>
+
+      <UserImageModal visible={isImageModalVisible} imageUrl={data?.avatar_url} onClose={() => setIsImageModalVisible(false)} />
+
+      {/* </SkeletonContent> */}
     </SafeAreaView >
   )
 }
