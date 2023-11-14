7f07dcf34a2168de122f8ecf1cea60d0883a831b
Furkan Şenol <frkn.snl@hotmail.com>
2023-11-15 01:52:40 +0300

    Update HomeStyles.ts

--- src/screens/Home/HomeStyles.ts
+++ src/screens/Home/HomeStyles.ts
@@ -1,7 +1,41 @@
 import { StyleSheet } from "react-native"
+import { statusBarHeight } from "../../utils/constants";
 
 const styles = StyleSheet.create({
-
-})
+    flex1: {
+        flex: 1
+    },
+    //header
+    headerContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingTop: statusBarHeight,
+        paddingHorizontal: 20,
+        paddingBottom: 20,
+        alignItems: 'center',
+        backgroundColor: '#0D1117'
+    },
+    headerText: {
+        fontSize: 18,
+        color: '#238636',
+        fontWeight: '600'
+    },
+    headerSortBy: {
+        color: 'white'
+    },
+    //Bottom Sheet
+    flatListStyle: {
+        zIndex: -1,
+        paddingBottom: 10,
+        //paddingTop:2
+    },
+    skeletonRow: {
+        flexDirection: 'row',
+        alignItems: 'flex-start',
+        justifyContent: 'space-between',
+        paddingHorizontal: 18,
+        paddingVertical: 10,
+    },
+});
 
 export default styles
